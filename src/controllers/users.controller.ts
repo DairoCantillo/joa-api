6aca028b99408e1ed4b6462a6619cd3263adde00
Dairo Cantillo <cantillodairo62@gmail.com>
2024-08-26 23:35:01 -0500

    update user controller to hash passwords before storing them

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
 import prismaClient from "../models/prismaClient";
+import config from "../config";
 
 export const createUser = async (req: Request, res: Response) => {
   try {
+    const { password, email, userName } = req.body;
+    const hashedPassword = await bcrypt.hash(password, config.bcryptSaltRounds);
+
     const newUser = await prismaClient.user.create({
-      data: req.body,
+      data: {
+        password: hashedPassword,
+        email,
+        userName,
+      },
     });
     res.status(201).json(newUser);
   } catch (error) {
@@ -26,15 +35,37 @@ export const updateUser = async (req: Request, res: Response) => {
   }
 };
 
-export const deleteUser = async (req: Request, res: Response) => {
+export const updateUserPassword = async (req: Request, res: Response) => {
   try {
-    const deletedUser = await prismaClient.user.delete({
+    const { password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, config.bcryptSaltRounds);
+    const updatedUser = await prismaClient.user.update({
       where: { id: req.body.id },
+      data: {
+        password: hashedPassword,
+      },
     });
-    res.status(200).json(deletedUser);
+    res.status(200).json(updatedUser);
   } catch (error) {
     console.error(error);
-    res.status(404).send("Not found");
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const userDeleted = await prismaClient.user.delete({
+      where: { id: req.body.id },
+    });
+    if (userDeleted) {
+      const { password, ...userWithoutPassword } = userDeleted;
+      res.status(200).json(userWithoutPassword);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
   }
 };
 
@@ -43,7 +74,12 @@ export const getUser = async (req: Request, res: Response) => {
     const user = await prismaClient.user.findUnique({
       where: { id: req.params.id },
     });
-    res.status(200).json(user);
+    if (user) {
+      const { password, ...userWithoutPassword } = user;
+      res.status(200).json(userWithoutPassword);
+    } else {
+      res.status(404).send("User not found");
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
